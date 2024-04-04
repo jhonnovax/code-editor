394f78c591bc9fa0e5798a2e5076e7f184bec7c4
Jhon Nova <jhonnovax@gmail.com>
2024-04-04 11:24:23 -0700

    Refactored home view component

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,6 @@
 <template>
   <h1>Code Editors</h1>
 
-  <nav>
-    <ul>
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/code-mirror">Code Mirror</RouterLink></li>
-      <li><RouterLink to="/monaco-editor">Monaco Editor</RouterLink></li>
-      <li><RouterLink to="/ace-editor">Ace Editor</RouterLink></li>
-      <li><RouterLink to="/syntax-highlighted">Syntax Highlighted</RouterLink></li>
-    </ul>
-  </nav>
-
   <main>
     <RouterView />
   </main>
--- src/home/home-view.vue
+++ src/home/home-view.vue
@@ -1,16 +1,55 @@
 <template>
-	<div ref="mainEl" class="syntax-highlighted">
-		Explore web code editors for CSS/HTML, so users can add this code at least with some syntax highlighting.
+	<div ref="mainEl" class="home-view">
+		<div class="editor-box">
+			<h1>Ace Editor</h1>
+			<ace-editor></ace-editor>
+		</div>
+		<div class="editor-box">
+			<h1>Code Mirror</h1>
+			<code-mirror-editor></code-mirror-editor>
+		</div>
+		<div class="editor-box">
+			<h1>Monaco Editor</h1>
+			<monaco-editor></monaco-editor>
+		</div>
+		<div class="editor-box">
+			<h1>Syntax Highlighted</h1>
+			<syntax-highlighted></syntax-highlighted>
+		</div>
 	</div>
 </template>
 
 <script>
+import AceEditor from '../ace-editor/ace-editor-view.vue';
+import CodeMirrorEditor from '../ace-editor/ace-editor-view.vue';
+import MonacoEditor from '../monaco-editor/monaco-editor-view.vue';
+import SyntaxHighlighted from '../syntax-highlighted/syntax-highlighted-view.vue';
+
 export default {
+	components: {
+		AceEditor,
+		CodeMirrorEditor,
+		MonacoEditor,
+		SyntaxHighlighted
+	}
 }
 </script>
 
 <style scoped>
-.syntax-highlighted {
+.home-view {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
 	padding: 10px;
+
+	.editor-box {
+		padding: 10px;
+
+		h1 {
+			font-size: 22px;
+			margin: 0;
+			margin-bottom: 10px;
+			color: purple;
+		}
+	}
 }
 </style>
