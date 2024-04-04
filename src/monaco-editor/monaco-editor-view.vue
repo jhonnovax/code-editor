f75298f1cd1af0183bdf9a3659f83a08b033b8d0
Jhon <jhonnovax@gmail.com>
2024-04-03 21:07:21 -0700

    Implemented monaco editor

--- src/monaco-editor/monaco-editor-view.vue
+++ src/monaco-editor/monaco-editor-view.vue
@@ -1,14 +1,28 @@
-<script setup>
+<script>
+import * as monaco from 'monaco-editor';
+
+export default {
+	mounted() {
+		monaco.editor.create(this.$refs.editor, {
+			value: ['<h1>Welcome</h1>', '<p>Hello World</p>'].join('\n'),
+			language: 'html'
+		});
+	}
+}
 </script>
 
 <template>
 	<div ref="mainEl" class="monaco-editor">
-		Monaco Editor
+		<div class="editor" ref="editor"></div>
 	</div>
 </template>
 
 <style scoped>
 .monaco-editor {
 	padding: 10px;
+
+	.editor {
+		height: 400px;
+	} 
 }
 </style>
