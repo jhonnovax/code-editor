cad302a09464aa5e03b51083dc9d8e4178d0a502
Jhon <jhonnovax@gmail.com>
2024-04-03 21:58:12 -0700

    Implemented syntax highlighet code editor

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,7 @@ nav {
     display: flex;
     justify-content: center;
     list-style: none;
+    margin: 0;
 
     li {
       padding: 10px;
--- src/syntax-highlighted/syntax-highlighed-view.vue
+++ src/syntax-highlighted/syntax-highlighed-view.vue
@@ -1,9 +1,19 @@
-<script setup>
+<script>
+export default {
+	mounted() {
+		const codeInputPlugins = [
+			new codeInput.plugins.Indent()
+		];
+		//codeInputTemplate = codeInput.templates.hljs(hljs, codeInputPlugins);
+		const codeInputTemplate = codeInput.templates.prism(Prism, codeInputPlugins);
+		codeInput.registerTemplate("syntax-highlighted", codeInputTemplate);
+	}
+}
 </script>
 
 <template>
 	<div ref="mainEl" class="syntax-highlighted">
-		Syntax highlighted
+		<code-input class="line-numbers" language="HTML"></code-input>
 	</div>
 </template>
 
