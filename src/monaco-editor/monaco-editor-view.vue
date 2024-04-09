73facee6b9d051cb0cf77e0f5be082838cf0bf33
Jhon Nova <jhonnovax@gmail.com>
2024-04-08 17:14:48 -0700

    Added plugin to load service worker in code editor

--- src/monaco-editor/monaco-editor-view.vue
+++ src/monaco-editor/monaco-editor-view.vue
@@ -1,9 +1,22 @@
 <script>
 import * as monaco from 'monaco-editor';
 import { htmlContent } from '../services/data-content';
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 
 export default {
 	mounted() {
+		window.MonacoEnvironment = {
+			getWorker: function (workerId, label) {
+				switch (label) {
+					case 'html':
+						return new HtmlWorker()
+					default:
+						return new EditorWorker()
+				}
+			}
+		};
+
 		monaco.editor.create(this.$refs.editor, {
 			value: htmlContent.join('\n'),
 			language: 'html'
